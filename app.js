749e26b60a717d55348b2445c9769f6da0c536e2
kshema25-hub <maheshgowdakl71@gmail.com>
2026-06-11 20:45:39 +0530

    Added backend files

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // configure express and middleware
+
+//import packages
+// create express app
+//               client => app => route =>response
+// configure middleware
+// middleware is the function that runs betn the req and response
+// req => middleware => Route => res
+// export the app
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+module.exports = app;
--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 // start the server
+
+const app=require("./app");
+const dotenv=require("dotenv");
+// load env variable
+dotenv.config({path:"./config.env"});
+// start server
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server is running on port ${process.env.PORT}`);
+})
