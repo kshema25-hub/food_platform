a5c0ba384f48c5a197ae489a8289b54d2bc5f984
kshema25-hub <maheshgowdakl71@gmail.com>
2026-06-11 21:04:01 +0530

    Updated backend project

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // start the server
 
-const app=require("./app");
-const dotenv=require("dotenv");
+const app = require("./app");
+const dotenv = require("dotenv");
 // load env variable
-dotenv.config({path:"./config.env"});
+dotenv.config({ path: "./config.env" });
 // start server
+PORT = process.env.PORT;
 
-app.listen(process.env.PORT,()=>{
-    console.log(`server is running on port ${process.env.PORT}`);
-})
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
