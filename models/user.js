676bb48afc4dfa83a2af167e0c7f0a3d6066698a
kshema25-hub <maheshgowdakl71@gmail.com>
2026-06-12 20:50:04 +0530

    commite backend files

--- models/user.js
+++ models/user.js
@@ -1,1 +1,28 @@
 // schema
+const mongoose = require("mongoose");
+
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+
+// step 2 create schema
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter your name"],
+        maxLength: [30, "Your name cannot exceed 30 characters"],
+        minLength: [4, "Your name must have at least 4 characters"],},
+    email: {
+        type: String,
+        required: [true, "Please enter your email"],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, "Please enter a valid email address"],
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter your password"],
+        minLength: [8, "Your password must have at least 8 characters"],
+        select: false,
+    },
