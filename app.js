6672dcbd20a2951d6ee95a4bb33627ef32607baa
kshema25-hub <maheshgowdakl71@gmail.com>
2026-06-13 20:45:25 +0530

    commite

--- app.js
+++ app.js
@@ -9,7 +9,9 @@
 // export the app
 const express = require("express");
 const app = express();
+const auth = require("./routes/auth");
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+app.use("/api/v1/users", auth);
 module.exports = app;
---
+++ controllers/authController.js
@@ -0,0 +1,42 @@
+// import required packages,files
+const user = require("../models/user");
+const errorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const sendToken = require("../utils/jwtToken");
+const cloudinary = require("cloudinary");
+const crypto = require("crypto");
+
+//signup
+exports.signup = catchAsyncErrors(async (req, res, next) => {
+    const { name, email, password, passwordConfirmation, phonenumber } = req.body;
+    let avatar = {};
+    // avator not provided during regisration
+    if (!req.body.avatar || req.body.avatar ==="/images/images.png") {
+        avatar = {
+            public_id: "default",
+            url: "/images/images.png"
+        }
+    }
+    else {
+        const result = await cloudinary.v2.uploader.upload(req.body.avatar, {
+            folder: "avatars",
+            width: 150,
+            crop: "scale",
+});
+        avatar = {
+            public_id: result.public_id,
+            url: result.secure_url
+
+
+        }    
+    }
+    const User = await user.create({
+        name,
+        email,
+        password,
+        passwordConfirmation,
+        phonenumber,
+        avatar
+    });
+    sendToken(User, 200, res);
+});
