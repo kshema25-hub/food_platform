33fff1f6b4a6a62ae22ce18174c1d929332b266f
kshema25-hub <maheshgowdakl71@gmail.com>
2026-07-03 07:50:04 +0530

    backend

--- controllers/paymentControlller.js
+++ controllers/paymentControlller.js
@@ -1,33 +1,69 @@
-const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
-const catchAsync = require("../middlewares/catchAsyncErrors");
+const catchAsyncErrors = require("../middlewares/catchAsyncErrors")
 
-const dotenv = require("dotenv");
-dotenv.config({ path: "backend/config/config.env" });
+const dotenv = require("dotenv")
+dotenv.config({path:"./config/config.env"})
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe =require("stripe")(process.env.STRIPE_SECRET_KEY)
+console.log("KEY",process.env.STRIPE_SECRET_KEY)
 
-console.log("KEY", process.env.STRIPE_SECRET_KEY);
 
-//process payment apt
-exports.processPayment = catchAsyncErrors(async (req, res, next) => {
-    console.log("REQ BODY", req.body);
+//process paymnet api
+exports.processPayment = catchAsyncErrors(async(req,res,next)=>{
+    console.log(req.body)
+
+    //create stripe checkout session
     const session = await stripe.checkout.sessions.create({
-        customer_email: req.body.email,
-        phone_number_collection: {
-            enabled: true,
+        customer_email: req.user.email,
+        phone_number_collection:{
+            enabled:true
         },
-        payment_method_types: ["card"],
-        line_items: req.body.cartItems.map((item) => ({
-            price_data: {
-                currency: "inr",
-                product_data: {
-                    name: item.name,
-                    images: [item.image[0].url]
-                    unit_amount: item.price * 100,
+        line_items: req.body.items.map((item) =>({
+            price_data:{
+                currency:"inr",
+                product_data:{
+                    name:item.foodItem.name,
+                    images:[item.foodItem.images[0].url],
                 },
-                quantity:item.quantity
+                //rs 100 becomes 10000 paise
+                unit_amount: item.foodItem.price * 100  
+            },
+             quantity:item.quantity
+        })),
+        mode:"payment",
+        shipping_address_collection:{
+            allowed_countries:["US", "IN"]
+        },
+        shipping_options:[
+            {
+                shipping_rate_data:{
+                    display_name:"Delivery Charges",
+                    type:"fixed_amount",
+                    fixed_amount:{
+                        amount:5500, //amount in paise (5500 => 55INR)
+                        currency:"inr"
+                    },
+                    delivery_estimate:{
+                        minimum:{
+                            unit:"hour",
+                            value:1
+                        },
+                        maximum:{
+                            unit:"hour",
+                            value:3
+                        }
+                    }
+                }
             }
-        }))
-    })
+        ],
+        success_url:`${process.env.FRONTEND_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.FRONTEND_URL}/cart`
+    });
+    res.status(200).json({url:session.url})
 })
 
+//send stripe api key
+exports.sendStripeApi = catchAsyncErrors(async(req,res,next)=>{
+res.status(200).json({
+    stripeApiKey: process.env.STRIPE_API_KEY
+})
+})
